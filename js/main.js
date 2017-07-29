3d527bd6f25c7cb9b43c29afbe43f5567c75cb65
Potoole99 <30417503+Potoole99@users.noreply.github.com>
2017-07-29 02:16:41 -0400

    Editted

--- js/main.js
+++ js/main.js
@@ -19,4 +19,26 @@ zingchart.render({
 	data : myConfig,
 	height: 750,
 	width: 1225,
+
+});
+var myConfig2 = {
+    "type":"line",
+    "plotarea":{
+
+
+    },
+    "csv":{
+      "url":"/Cleaned Data Set2.csv",
+      "vertical-labels":true
+    }
+
+
+
+};
+
+zingchart.render({
+	id : 'Chart2',
+	data : myConfig2,
+	height: 750,
+	width: 1225,
 });
